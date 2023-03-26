2a0baadf1ddf4e9ee98b5fa96ea91be2acdf5a04
solyanov_ye <yurasneo@yandex.ru>
2023-03-26 13:51:57 +0300

    реализована функция -> принятие имени файла и вывод его расширения

--- main.py
+++ main.py
@@ -5,7 +5,11 @@ def get_extension(file_name: str) -> str:
     :return:
             str: строка, расширение файла
     """
-
+    file_list = file_name.split(".")
+    if len(file_list) <= 1:
+        raise Exception(f"Файл '{file_list}' не имеет расширения!")
+    else:
+        return "." + file_list[-1]
 
 def execute_application():
     pass
