4372a3772a63188357d5e3df25445c240e16e46f
solyanov_ye <yurasneo@yandex.ru>
2023-03-26 13:45:03 +0300

    описана функция -> принятие имени файла и вывод его расширения

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
+def get_extension(file_name: str) -> str:
+    """
+    Возвращает расширение файла в виде строки
+    :param file_name (str): строка, название файла
+    :return:
+            str: строка, расширение файла
+    """
+
 
 def execute_application():
     pass
