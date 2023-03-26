14bd503c93e6913ef4450c3477d1c024d4e4672b
solyanov_ye <yurasneo@yandex.ru>
2023-03-26 14:45:00 +0300

    вызов функции get_extension и вывод расширения файла на экран

--- main.py
+++ main.py
@@ -9,13 +9,17 @@ def get_extension(file_name: str) -> str:
     if len(file_list) <= 1:
         raise Exception(f"Файл '{file_list}' не имеет расширения!")
     else:
-        return "." + file_list[-1]
+        return file_list[-1]
 
 
 def execute_application():
     file_name = input("Введите название файла с расширением: ")
+    try:
+        extension = get_extension(file_name)
+        print(f"Файл '{file_name}' имеет расширение '{extension}'")
+    except Exception as e:
+        print(e)
 
 
 if __name__ == "__main__":
     execute_application()
-
